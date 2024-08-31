569453801c18ea787ff6058a95963dca5df94412
Dmitry <whywhymeow@gmail.com>
2024-08-30 17:48:40 -0700

    Accuracy increase Got accuracy up to 86% by training with both a more verbally complex dataset alongside a more simplistic one, which seems to have some the bot well. I'm pleased enough with that rating. It appears to struggle with neutrality, which is to be expected, considering it's a bit more difficult to find and correctly categorize truly neutral reviews. I'll work on getting it some good data to train on for said neutrality, and may even re-evaluate the way that it determines sentiment... I'm thinking that, instead of taking simple 0s, 1s, and 2s, I'll get some datasets that are rated via stars; that way, it can weigh out the positivity and negativity of a statement in a range and then decide whether the statement is negative, mostly negative, neutral, mostly positive, or positive.

--- train_model.py
+++ train_model.py
@@ -3,12 +3,15 @@ import pandas as pd
 import string
 import re
 import random
+
+from Tools.scripts.objgraph import printundef
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, accuracy_score
 
 # Initializations for preprocessing.
@@ -23,7 +26,8 @@ def preprocess_text(text):
     text = text.translate(str.maketrans('', '', string.punctuation))
     # Remove numbers.
     text = re.sub(r'\d+', '', text)
-    # Remove extra whitespaces.
+    # Remove unwanted characters.
+    text = re.sub(r'[^\x00-\x7F]+', '', text)
     text = ' '.join(text.split())
     # Tokenize.
     tokens = word_tokenize(text)
@@ -37,16 +41,25 @@ def preprocess_text(text):
     return processed_text
 
 # Read and preprocess data.
-mcdonalds_reviews = pd.read_csv("./train/reviews_mcdonalds_labelled.csv")
-hotel_reviews = pd.read_csv("./train/reviews_hotels_labelled.csv")
-reviews_df = pd.concat([mcdonalds_reviews, hotel_reviews], ignore_index=True)
-reviews_df['Preprocessed'] = reviews_df['Review'].apply(preprocess_text)
+# Training dataframe.
+reviews_1 = pd.read_csv("./training/train/reviews_amazon_labelled.csv")
+reviews_2 = pd.read_csv("./training/train/reviews_restaurants_labelled.csv")
+training_df = pd.concat([reviews_1, reviews_2], ignore_index=True)
+training_df = training_df.iloc[5000:]
+training_df['Preprocessed'] = training_df['Review'].apply(preprocess_text)
+
+# Testing dataframe.
+reviews_3 = pd.read_csv("./training/test/reviews_hotels_labelled.csv")
+reviews_4 = pd.read_csv("./training/test/reviews_mcdonalds_labelled.csv")
+testing_df = pd.concat([reviews_3, reviews_4], ignore_index=True)
+testing_df = testing_df.iloc[5000:]
+testing_df['Preprocessed'] = testing_df['Review'].apply(preprocess_text)
 
 # Initializations for training.
-model = LogisticRegression()
-X = reviews_df['Preprocessed']
-y = reviews_df['Sentiment']
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+model = LogisticRegression(max_iter=200)
+X = training_df['Preprocessed']
+y = training_df['Sentiment']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1, stratify=y)
 
 # Vectorize.
 vectorizer = CountVectorizer()
@@ -60,12 +73,17 @@ model.fit(X_train_vec, y_train)
 y_pred = model.predict(X_test_vec)
 features_nd = X_test_vec.toarray()
 
-for i in range(5):
+print(training_df['Sentiment'].value_counts())
+print(testing_df['Sentiment'].value_counts())
+print()
+
+for i in range(10):
     idx = random.randint(0, len(X_test) - 1)
-    print(f"Review: {reviews_df['Review'].iloc[idx]}")
+    print(f"Review: {testing_df['Review'].iloc[idx]}")
+    print(f"Preprocessed: {testing_df['Preprocessed'].iloc[idx]}")
     print(f"Predicted Sentiment: {y_pred[idx]}")
-    print(f"Actual Sentiment: {y_test.iloc[idx]}")
+    print(f"Actual Sentiment: {testing_df['Sentiment'].iloc[idx]}")
     print()
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred, labels=[0, 2], target_names=['Negative', 'Positive']))
