de8c93d67641d2d8ed8557421d888eca329788bb
Dmitry <whywhymeow@gmail.com>
2024-09-02 16:01:31 -0700

    Handling negations Implemented a function to handle negations, which jumped the accuracy score to 55%.

--- train_model.py
+++ train_model.py
@@ -10,14 +10,34 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report, accuracy_score
 
 # Initializations for preprocessing.
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
-# Preprocessing function.
+# Function to help handle negations.
+def handle_negations(tokens):
+    # Initialization.
+    negation_words = {"not", "no", "never", "n't"}
+    new_tokens = []
+    negation = False
+
+    # For each token, determine is word is being negated.
+    for token in tokens:
+        if token in negation_words:
+            negation = True
+        elif negation:
+            # Apply negation to the following words
+            new_tokens.append("NOT_" + token)
+            negation = False
+        else:
+            new_tokens.append(token)
+
+    return new_tokens
+
+# Function to preprocess text.
 def preprocess(text):
     # Lowercase.
     text = text.lower()
@@ -28,17 +48,17 @@ def preprocess(text):
     # Remove unwanted characters.
     text = re.sub(r'[^\x00-\x7F]+', '', text)
     text = ' '.join(text.split())
-    # Tokenize.
+    # Tokenize and ensure negations are handled.
     tokens = word_tokenize(text)
+    tokens = handle_negations(tokens)
     # Remove stopwords.
     tokens = [word for word in tokens if word not in stop_words]
-    # Lemmatize words.
+    # Lemmatize and stem words.
     lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
     # Join words back into a single string.
     processed_text = ' '.join(lemmatized_words)
     # Return.
     return processed_text
-
 
 # Function to determine sentiment.
 def rating_to_sentiment(rating):
@@ -49,9 +69,9 @@ def rating_to_sentiment(rating):
     elif rating == 3:
         return 'neutral'
     elif rating == 4:
-        return 'mostly postive'
+        return 'mostly positive'
     elif rating == 5:
-        return 'postive'
+        return 'positive'
 
 print('Reading and preprocessing training data...')
 
@@ -83,7 +103,7 @@ y = training_df['Rating']
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
 
 # Vectorize.
-vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
