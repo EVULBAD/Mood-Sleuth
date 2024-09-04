87699807d9144649df8a28d83337fcc20c504813
Dmitry <whywhymeow@gmail.com>
2024-09-04 01:25:44 -0700

    Web app routing Web app routes correctly in every way. Text inputs are assessed on individual bases, and CSVs are properly handled then offered for download.

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-# Imports.
 import pandas as pd
 import os
 import joblib
+import random
 
-from flask import Flask, render_template, request, send_file
-from sentiment_analysis import preprocess, rating_to_sentiment
+from flask import Flask, render_template, request, jsonify, send_file, after_this_request
+from datetime import datetime
 from io import BytesIO
+from sentiment_analysis import preprocess, rating_to_sentiment
 
-# Load the model and vectorizer
+# Load the model and vectorizer.
 logreg_model = joblib.load('sentiment_model.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
 
-# Define upload folder path.
+# Definitions.
 UPLOAD_FOLDER = 'uploads'
+TEMP_FOLDER = 'temp'
 ALLOWED_EXTENSIONS = {'csv'}
+global_variables = {}
 
 # Create app.
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TEMP_FOLDER'] = TEMP_FOLDER
 
 # Function to check that upload is csv.
 def allowed_file(filename):
@@ -25,87 +29,78 @@ def allowed_file(filename):
 
 # Function to create a unique filename if the file already exists in the directory.
 def get_unique_filename(filepath):
-    # Split the file path into base and extension.
     base, extension = os.path.splitext(filepath)
     counter = 1
-
-    # Check if file exists, and append a number to the base if it does.
     while os.path.exists(filepath):
-        filepath = f'{base}_{counter}{extension}'
+        filepath = f"{base}_{counter}{extension}"
         counter += 1
-
     return filepath
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Check if the post request has the file part.
         if 'file' in request.files:
             file = request.files['file']
-            # If user does not select file, browser may also submit an empty part without filename.
             if file.filename == '':
-                return 'No selected file. Please upload csv.'
+                return jsonify({'error': 'No selected file. Please upload csv.'})
             if file and allowed_file(file.filename):
                 filename = file.filename
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file_path = get_unique_filename(file_path)
                 file.save(file_path)
                 df = pd.read_csv(file_path)
-                df = df.drop_duplicates(subset=['Review'])
-                
-                # Perform sentiment analysis.
                 df['Preprocessed'] = df['Review'].apply(preprocess)
                 X_vec = vectorizer.transform(df['Preprocessed'])
                 df['Rating'] = logreg_model.predict(X_vec)
                 mean_predicted_rating = df['Rating'].mean()
                 avg_sentiment = rating_to_sentiment(round(mean_predicted_rating))
-                example_positive_review = df[df['Rating'] == 5].sample(1)['Review'].values[0] if not df[df['Rating'] == 5].empty else 'No positive reviews found.'
-                example_negative_review = df[df['Rating'] == 1].sample(1)['Review'].values[0] if not df[df['Rating'] == 1].empty else 'No negative reviews found.'
-                
-                # Prepare CSV for download.
                 sentiment_analysis_report = df[['Review', 'Rating']].to_csv(index=False)
 
-                # Delete file after analysis.
+                # Generate unique filename using current time and a random 2 digits.
+                dt = datetime.now()
+                dt = dt.strftime('%H%M%S%f')[:-3]
+                rand = random.random()
+                rand = round(rand * 100)
+                rand = str(rand)
+                output_name = 'mood_sleuth_' + dt + rand + '.csv'
+                global_variables['output_file'] = output_name
+
+                sentiment_analysis_report_path = os.path.join(app.config['TEMP_FOLDER'], output_name)
+                df[['Review', 'Rating']].to_csv(sentiment_analysis_report_path, index=False)
+
+                # Delete file after processing.
                 os.remove(file_path)
+
+                return jsonify({
+                    'avg_sentiment': avg_sentiment,
+                    'sentiment_analysis_report': sentiment_analysis_report
+                })
             else:
-                return 'File invalid. Please only upload csv.'
+                return jsonify({'error': 'File invalid. Please only upload csv.'})
+
         elif 'text_input' in request.form:
-            # Text input handling.
             input_text = request.form['text_input']
             preprocessed_text = preprocess(input_text)
             input_vector = vectorizer.transform([preprocessed_text])
-            predicted_rating_int = logreg_model.predict(input_vector)[0]
+            predicted_rating_int = int(logreg_model.predict(input_vector)[0])  # Convert to native int
             predicted_rating_text = rating_to_sentiment(predicted_rating_int)
 
-            # Render the result on the same page.
-            return render_template('index.html', input_text=input_text, predicted_rating_int=predicted_rating_int, predicted_rating_text=predicted_rating_text)
-
-        return render_template(
-            'index.html',
-            avg_sentiment = avg_sentiment,
-            example_positive_review = example_positive_review,
-            example_negative_review = example_negative_review,
-            sentiment_analysis_report = sentiment_analysis_report
-        )
+            return jsonify({
+                'input_text': input_text,
+                'predicted_rating_int': predicted_rating_int,
+                'predicted_rating_text': predicted_rating_text
+            })
 
     return render_template('index.html')
 
-@app.route('/download_predictions', methods=['POST'])
-def download_predictions():
-    # Retrieve the CSV content from the form.
-    csv_content = request.form.get('sentiment_analysis_report')
-
-    # Prepare the CSV file for download.
-    output = BytesIO()
-    output.write(csv_content.encode('utf-8'))
-    output.seek(0)
-
-    return send_file(
-        output,
-        as_attachment=True,
-        download_name='sentiment_analysis_report.csv',
-        mimetype='text/csv'
-    )
+@app.route('/download_temp', methods=['GET'])
+def download_temp():
+    output_name = global_variables.get('output_file')
+    output_path = os.path.join(app.config['TEMP_FOLDER'], output_name)
+    if os.path.exists(output_path):
+        return send_file(output_path, as_attachment=True)
+    else:
+        return "File not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/AJAX.js
+++ static/AJAX.js
@@ -5,38 +5,61 @@ document.addEventListener('DOMContentLoaded', function() {
     const csvResult = document.getElementById('csvResult');
     const loadingDiv = document.getElementById('loading');
 
+    function pickColor(avgSentiment) {
+        if (avgSentiment === 'negative') {
+            span_color = "#ff002f"
+        } else if (avgSentiment === 'mostly negative') {
+            span_color = "#ff51e9"
+        } else if (avgSentiment === 'neutral') {
+            span_color = "#2941ff"
+        } else if (avgSentiment === 'mostly positive') {
+            span_color = "#00C3FF"
+        } else {
+            span_color = "#00FF2C"
+        }
+
+        return span_color
+    }
+
     function handleResponse(data) {
         if (data.input_text) {
+            span = pickColor(data.predicted_rating_text)
             textResult.innerHTML = `
-                        <h2>Result</h2>
-                        <p>Input Text: ${data.input_text}</p>
-                        <p>This text appears to be ${data.predicted_rating_text}.</p>
-                    `;
+                <h2>Result</h2>
+                <p>This text is <span style="border-bottom: dotted 2px ${span_color}">${data.predicted_rating_text}</span>.</p>
+            `;
             textResult.classList.remove('hidden');
+            textResult.classList.add('show');
+            setTimeout(() => {
+                textResult.classList.remove('show');
+                textResult.classList.add('hidden');
+            }, 7000);
         }
 
         if (data.avg_sentiment) {
+            span = pickColor(data.avg_sentiment)
             csvResult.innerHTML = `
-                        <h2>CSV Analysis Summary</h2>
-                        <p>This file contains mostly ${data.avg_sentiment} reviews.</p>
-                        <p>Here's a positive review: ${data.example_positive_review}</p>
-                        <p>And here's a negative one: ${data.example_negative_review}</p>
-                        <p>For a detailed report, please download and review the following csv:</p>
-                        <form action="/download_predictions" method="post">
-                            <input type="hidden" name="sentiment_analysis_report" value="${data.sentiment_analysis_report}">
-                            <input type="submit" value="Download Predictions CSV">
-                        </form>
-                    `;
+                <h2>Result</h2>
+                <p>This file contains a majority of <span style="border-bottom: dotted 2px ${span_color}">${data.avg_sentiment}</span> reviews.</p>
+                <form class="bottom" action="/download_temp" method="get">
+                    <input type="submit" value="Download Full Report">
+                </form>
+            `;
             csvResult.classList.remove('hidden');
+            csvResult.classList.add('show');
         }
 
+        loadingDiv.classList.remove('show');
         loadingDiv.classList.add('hidden');
     }
 
     function submitForm(form, event) {
         event.preventDefault();
         loadingDiv.classList.remove('hidden');
+        loadingDiv.classList.add('show');
+        textResult.classList.remove('show');
         textResult.classList.add('hidden');
+        csvResult.classList.remove('show');
         csvResult.classList.add('hidden');
 
         const formData = new FormData(form);
@@ -46,7 +69,11 @@ document.addEventListener('DOMContentLoaded', function() {
         })
             .then(response => response.json())
             .then(data => handleResponse(data))
-            .catch(error => console.error('Error:', error));
+            .catch(error => {
+                console.error('Error:', error);
+                loadingDiv.classList.remove('show');
+                loadingDiv.classList.add('hidden');
+            });
     }
 
     textForm.addEventListener('submit', function(event) {
